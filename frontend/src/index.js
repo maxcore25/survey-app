c1f92f5f965959615fc1cd51307cd4dc382d60ae
Max <mkorobov2002@mail.ru>
2022-11-22 15:53:39 +0300

    Global Styles Imported

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./hooks/useAuth";
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './hooks/useAuth';
+import './styles/index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-          <App />
+        <App />
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
