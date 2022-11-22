eb9409af8e629264095a6d2ad5e549f970d88ab0
Max <mkorobov2002@mail.ru>
2022-11-22 21:09:22 +0300

    SurveyCard Styles Fixed

--- frontend/src/components/elements/cards/SurveyCard/index.js
+++ frontend/src/components/elements/cards/SurveyCard/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import AppButton from '../../buttons/AppButton';
 import { styled } from '@mui/material/styles';
 import { Stack } from '@mui/system';
+import { Link } from 'react-router-dom';
 
 const Theme = styled(Typography)({
   textTransform: 'uppercase',
@@ -11,17 +12,14 @@ const Theme = styled(Typography)({
   fontSize: '16px',
   lineHeight: '18px',
   color: '#A6A8B5',
-  marginBottom: '16px',
 });
 
 const Title = styled(Typography)({
-  textTransform: 'uppercase',
   fontFamily: 'inherit',
   fontWeight: 400,
   fontSize: '14px',
   lineHeight: '16px',
   color: '#2B2B2C',
-  marginBottom: '18px',
 });
 
 export default function SurveyCard() {
@@ -29,17 +27,27 @@ export default function SurveyCard() {
     <Paper
       elevation={0}
       sx={{ borderRadius: '12px', border: '1px solid #D9DDE0' }}>
-      <Theme>ТРАНСПОРТ</Theme>
-      <Title>Викторина «Современные автовокзалы Москвы»</Title>
+      <Stack sx={{ p: 2, marginBottom: '8px', gap: '16px' }}>
+        <Theme>ТРАНСПОРТ</Theme>
+        <Title>Викторина «Современные автовокзалы Москвы»</Title>
+      </Stack>
       <CardMedia
         component='img'
         height='160'
         image='https://kipmu.ru/wp-content/uploads/klvrgfg-scaled.jpg'
         alt='survey'
       />
-      <Stack direction='row'>
-        <AppButton>Голосовать</AppButton>
-        <span>+20 баллов</span>
+      <Stack
+        direction='row'
+        sx={{ p: 2, alignItems: 'center', justifyContent: 'space-between' }}>
+        <Link to='/survey/1'>
+          <AppButton>Голосовать</AppButton>
+        </Link>
+        <Typography
+          component='span'
+          sx={{ color: 'var(--color-primary)', fontFamily: 'inherit' }}>
+          +20 баллов
+        </Typography>
       </Stack>
     </Paper>
   );
