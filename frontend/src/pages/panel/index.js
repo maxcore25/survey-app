0dc67c9ca23c4ae84bfc839518a47c4b8652b9aa
Max <mkorobov2002@mail.ru>
2022-11-23 12:53:31 +0300

    PanelLayout Imported

--- frontend/src/pages/panel/index.js
+++ frontend/src/pages/panel/index.js
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import AppLayout from '../../components/layouts/AppLayout';
+import PanelLayout from '../../components/layouts/PanelLayout';
 
 export default function Panel() {
   useEffect(() => {
     console.log('Component did mount');
   }, []);
 
-  return <AppLayout>Panel</AppLayout>;
+  return (
+    <AppLayout>
+      <PanelLayout />
+    </AppLayout>
+  );
 }
