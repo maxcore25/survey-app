25bf06023d93695cdf5ee9e658f89b21ac68a52f
Max <mkorobov2002@mail.ru>
2022-11-23 13:11:24 +0300

    RecordBookTable Deleted

--- frontend/src/components/layouts/PanelLayout/index.js
+++ frontend/src/components/layouts/PanelLayout/index.js
@@ -1,12 +1,10 @@
 import { Stack } from '@mui/material';
 import React from 'react';
-import RecordBookTable from '../../elements/tables/RecordBookTable';
 import AllRecordBooksTable from '../../elements/tables/AllRecordBooksTable';
 
 export default function PanelLayout() {
   return (
     <Stack sx={{ gap: '32px', m: '64px auto', maxWidth: 'var(--max-width)' }}>
-      <RecordBookTable />
       <AllRecordBooksTable />
     </Stack>
   );
