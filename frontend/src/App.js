9480928211b61643934c88b6b43c41f947e12b42
Max <mkorobov2002@mail.ru>
2022-11-24 16:35:03 +0300

    MainContextProvider Imported

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,45 +6,48 @@ import Home from './pages/home';
 import ProtectedRoute from './components/elements/auth/ProtectedRoute';
 import Survey from './pages/survey';
 import Panel from './pages/panel';
+import { MainContextProvider } from './context/MainContextProvider';
 
 function App() {
   return (
-    <Routes>
-      <Route exact path='/' element={<SignIn />} />
-      <Route exact path='/register' element={<SignUp />} />
-      <Route
-        path='/teacher'
-        element={
-          <ProtectedRoute>
-            <SignIn />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path='/home'
-        element={
-          <ProtectedRoute>
-            <Home />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path='/survey/1'
-        element={
-          <ProtectedRoute>
-            <Survey />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path='/panel'
-        element={
-          <ProtectedRoute>
-            <Panel />
-          </ProtectedRoute>
-        }
-      />
-    </Routes>
+    <MainContextProvider>
+      <Routes>
+        <Route exact path='/' element={<SignIn />} />
+        <Route exact path='/register' element={<SignUp />} />
+        <Route
+          path='/teacher'
+          element={
+            <ProtectedRoute>
+              <SignIn />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/home'
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/survey/1'
+          element={
+            <ProtectedRoute>
+              <Survey />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/panel'
+          element={
+            <ProtectedRoute>
+              <Panel />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </MainContextProvider>
   );
 }
 
