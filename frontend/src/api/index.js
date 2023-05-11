beaa44686204256c8b11b4542418be3cb4ddfe0c
Dragonprod <artyom2002-98@mail.ru>
2023-05-11 11:40:18 +0300

    fix: CI/CD

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8080/api',
+  baseURL: process.env.REACT_APP_API_URL || 'http://62.109.29.32:8080/api',
   headers: {
     Authorization: `Bearer ${localStorage.getItem("access_token")}`
   }
